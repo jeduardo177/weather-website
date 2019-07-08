8dd434518eba9fd17ab5a1164d1769b46cc67fff
Jose Eduardo Pereira <jose.pereira@sngular.com>
2019-07-08 08:33:05 +0200

    change port and urls

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit', (event) => {
         return console.log('Please introduce a location')
     }
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
         response.json().then((data) =>{
             if (data.error) {
                 return  messageOne.textContent = data.error
